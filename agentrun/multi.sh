c323bf6d8537836b4d853c628fb7fa9548e0a634
qsupremacy <55643774@qq.com>
2026-06-27 08:37:37 +0800

    delete old log

--- agentrun/multi.sh
+++ agentrun/multi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 1 500); do
+for i in $(seq 1 100); do
   #agentarts invoke '{"message": "hello,world"}' --session haolipeng &
   
   time curl https://1916215180918953.agentrun-data.cn-shanghai.aliyuncs.com/agent-runtimes/agent-code-AawC7/endpoints/Default/invocations/openai/v1/chat/completions -XPOST \
